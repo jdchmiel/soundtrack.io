ddb9a0d4d22ad1c1287f6690f65d1ecd3833bde7
Jared Chmielecki <jdchmiel@gmail.com>
2014-11-18 04:39:21 +0000

    Make an X for top right of inline chat imags that when clicked removes the image. Make the Artist, tracks, and plays listings of tracks use a similar table layout with alternation for better visibility and a more uniform button set for actions on a track (edit, enqueue, delete, add to playlist)

--- public/js/imageTagger.js
+++ public/js/imageTagger.js
@@ -2,15 +2,10 @@
 // haystack is a root level node, no tree traversal this iteration
 
 var imageTagger = (function() { //set up scope wrapper for this plugin
-/*	var function doClose() {
-	}
+	$(document).on("click","div.inlineChatImage span.bigX", function(event) {
+		 $(this).parent().remove();
+	});
 
-	var addClosers($haystack) {
-		$hayStack.each('img', function(index, value){
-			
-		});
-	}
-*/
 	var closerString = "<span class='bigX'>X</span>";
 
 	// export function outside scope wrapper
@@ -20,10 +15,10 @@ var imageTagger = (function() { //set up scope wrapper for this plugin
 			var matches = hayString.match(/(http(s)?:\/\/.*\.(?:jpg|gif|png|jpeg))/ig);
 			if (matches && matches.length > 0){
 				for (match in matches) {
-					taggedString = hayString.replace(matches[match], "<img class='inlineChatImage' src='" + matches[match] + "' />");
+					taggedString = hayString.replace(matches[match], "<div class='inlineChatImage'><img src='" + matches[match] + "' /></div>");
 				}	
 				$hayStack.html(taggedString);
-//				this.addClosers($hayStack);
+				$hayStack.find('img').before($(closerString));
 			}
 		}
 	}
